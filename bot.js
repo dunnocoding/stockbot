b6c59417de271c86841378827833eb714a02a318
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2021-01-30 09:25:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,21 @@
 let Discord = require('discord.js');
 let request = require('request');
 let cheerio = require('cheerio');
-var bot = new Discord.Client();
+let bot = new Discord.Client();
 
 bot.on('ready', function() {
     console.log(`Logged in as ${bot.client.username} at ${new Date().toLocaleString()}`);
 });
 
 bot.on('message', function(message) {
-	var rxp = new RegExp(/^(asx:)[a-z0-9]{3}/i);
-	if (rxp.test(message)){
-		var market = message.substr(0,3).toUpperCase();
-		var code = message.substr(4,3).toUpperCase();
-		var url = "https://finance.google.com/finance/getprices?p=1d&i=60&x=ASX&f=c&q="+code;
-		console.log(url);
-		scrape(url,message.channel,code,market);
-	}
+// 	var rxp = new RegExp(/^(asx:)[a-z0-9]{3}/i);
+// 	if (rxp.test(message))
+		let url = "https://finance.google.com/finance/?q=GME";
+		scrape(url,message.channel);
 });
 bot.login(process.env.TOKEN);
 
-function scrape(url,channel,code,market){
+function scrape(url,channel){
     request(url, function(error, response, html){
 		console.log('error:', error); // Print the error if one occurred
 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
