cb4cf5e12929fd23f101cd83cf06ca65fe77229e
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2021-01-30 09:20:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,54 +1,29 @@
-var Discord = require('discord.io');
-var auth = require('./credentials.json');
-var fs = require('fs');
-var request = require('request');
-var cheerio = require('cheerio');
-
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
+let Discord = require('discord.js');
+let request = require('request');
+let cheerio = require('cheerio');
+var bot = new Discord.Client();
 
 bot.on('ready', function() {
-    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+    console.log(`Logged in as ${bot.client.username} at ${new Date().toLocaleString()}`);
 });
 
-bot.on('message', function(user, userID, channelID, message, event) {
+bot.on('message', function(message) {
 	var rxp = new RegExp(/^(asx:)[a-z0-9]{3}/i);
 	if (rxp.test(message)){
 		var market = message.substr(0,3).toUpperCase();
 		var code = message.substr(4,3).toUpperCase();
 		var url = "https://finance.google.com/finance/getprices?p=1d&i=60&x=ASX&f=c&q="+code;
 		console.log(url);
-		scrape(url,channelID,code,market);
+		scrape(url,message.channel,code,market);
 	}
 });
+bot.login(process.env.TOKEN);
 
-function scrape(url,channelID,code,market){
+function scrape(url,channel,code,market){
     request(url, function(error, response, html){
 		console.log('error:', error); // Print the error if one occurred
 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        var reg = new RegExp(/[a-zA-Z]/);
-		if(!error){
-            var rows = html.split(/\r?\n/);
-			if(!reg.test(rows[rows.length - 2])){
-				var price = "**"+code+"** is currently trading for **$"+rows[rows.length - 2]+"** on the **"+market+"**";
-				console.log("Price: "+price);
-				bot.sendMessage({
-					to: channelID,
-					message: price
-				});
-			}else{
-				bot.sendMessage({
-				to: channelID,
-				message: "Invalid Stock"
-			});
-			}
-        }else{
-			bot.sendMessage({
-				to: channelID,
-				message: "Failed to get price"
-			});
-		}
-	});
+	    let $ = cheerio.load(response);
+	    priceGME = $(span).attr('jsname','vWLAgc').text();
+    }).catch(err => console.log(err);new Discord.Channel(channel.send(err)););
 }
