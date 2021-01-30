269f119d719cafde994f04a88637074e19407198
christusdsouza <46989701+christusdsouza@users.noreply.github.com>
2021-01-30 16:58:37 +0530

    :p

--- bot.js
+++ bot.js
@@ -1,25 +1,66 @@
+require('dotenv').config();
 let Discord = require('discord.js');
-let request = require('request');
+let axios = require('axios');
 let cheerio = require('cheerio');
+let fs = require('fs');
 let bot = new Discord.Client();
+//let polltime = 300000;
+//let timer;
 
 bot.on('ready', function() {
-    console.log(`Logged in as ${bot.client.username} at ${new Date().toLocaleString()}`);
+    console.log(`Logged in as ${bot.user.username} at ${new Date().toLocaleString()}`);
+    bot.user.setPresence({
+        activity: {
+            name: '$toink$', type: 'COMPETING',
+        },
+        status: 'online',
+    });
+    setInterval(scrape, polltime=60000, {});
 });
 
 bot.on('message', function(message) {
-// 	var rxp = new RegExp(/^(asx:)[a-z0-9]{3}/i);
-// 	if (rxp.test(message))
-		let url = "https://finance.google.com/finance/?q=GME";
-		scrape(url,message.channel);
+    if(message.author.bot && !message.content.match(new RegExp(`^(<@(|!)${bot.user.id}+>)`,'g'))) return;
+    switch(arg=message.content.split(' ')[1]) {
+        case 'track':
+            scrape({channel: message.channel});break;
+        case 'ua': case 'header':
+            scrape({channel: message.channel, ua: arg});
+            message.reply('UA changed!');
+    }
+});
+bot.on('ratelimit', function(info) {
+    bot.user.setPresence({
+        activity: {
+            name: 'Bonkers, We\'ve hit a ratelimit!!!', type: 'LISTENING',
+        },
+        status: 'idle',
+    });
+    console.log(`Timeout:${info.timeout}	| Limit: ${ info.limit} | Method: ${info.method} | Path: ${info.path} | Route: ${info.route}`);
 });
 bot.login(process.env.TOKEN);
 
-function scrape(url,channel){
-    request(url, function(error, response, html){
-		console.log('error:', error); // Print the error if one occurred
-		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	    let $ = cheerio.load(response);
-	    priceGME = $(span).attr('jsname','vWLAgc').text();
-    }).catch(err => console.log(err);new Discord.Channel(channel.send(err)););
-}
+function scrape({
+    channel,
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}){
+    axios.get('https://finance.google.com/finance/?q=GME',{ headers: {'User-Agent': ua }})
+    .then(function (res) { 
+        let $ = cheerio.load(res.data);
+        priceGME = $('span[jsname=vWLAgc]').first().text();
+        subtitle = $('div[data-attrid=subtitle]').text().split(':');
+        
+        if(channel) {
+            channel.guild.members.cache.get(bot.user.id).setNickname(`[${subtitle.join(']')}: ${priceGME}`).catch(err => channel.send(`If you cant see my name update it's because of ${err.message}`));
+            return channel.send(`[${subtitle.join(']')}: ${priceGME}`);
+        }
+        bot.guilds.cache.each(guild => guild.members.cache.get(bot.user.id).setNickname(`[${subtitle.join(']')}: ${priceGME}`));
+    }).catch(err =>{
+        bot.user.setPresence({
+            activity: {
+                name: err.message ,type: 'WATCHING',
+            },
+            status: 'dnd',
+        });
+        console.log(err.message);
+    });
+    }
+    
